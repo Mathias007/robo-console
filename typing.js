063f097780ee13e83ccd064642ab975e913089b8
Mathias007 <mathias.najlepszy@gmail.com>
2020-02-23 19:05:37 +0100

    Extend typing using requestAnimationFrame()

--- typing.js
+++ typing.js
@@ -1,13 +1,18 @@
 const box = document.querySelector(".typing");
-
 const text = "No dzień dobry, ugułem...";
 
 let wordIndex = 0;
+let oldTime = 0;
+let speed = 100; // im większa wartość, tym wolniejsze pisanie
 
-const typing = () => {
-    const letter = text.substr(wordIndex, 1);
-    box.textContent += letter;
-    wordIndex++;
+const typing = newTime => {
+    if (newTime - oldTime > speed) {
+        oldTime = newTime;
+        const letter = text.substr(wordIndex, 1);
+        box.textContent += letter;
+        wordIndex++;
+    }
+    requestAnimationFrame(typing);
 };
 
 typing();
