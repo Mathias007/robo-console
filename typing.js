32f346cd3cdb3201a014086ea31812d3e603def9
Mathias007 <mathias.najlepszy@gmail.com>
2020-02-23 19:12:33 +0100

    Handle typing many texts in array.

--- typing.js
+++ typing.js
@@ -1,14 +1,27 @@
 const box = document.querySelector(".typing");
-const text = "No dzień dobry, ugułem...";
+const text = [
+    "No dzień dobry, ugułem...",
+    "O... właśnie....",
+    "CZEGO TY KRZYCZYSZ KNURZE?!"
+];
 
 let wordIndex = 0;
+let textIndex = 0;
 let oldTime = 0;
 let speed = 100; // im większa wartość, tym wolniejsze pisanie
 
 const typing = newTime => {
+    if (wordIndex === text[textIndex].length) {
+        return setTimeout(() => {
+            box.textContent = "";
+            textIndex++;
+            wordIndex = 0;
+            requestAnimationFrame(typing);
+        }, 2000);
+    }
     if (newTime - oldTime > speed) {
         oldTime = newTime;
-        const letter = text.substr(wordIndex, 1);
+        const letter = text[textIndex].substr(wordIndex, 1);
         box.textContent += letter;
         wordIndex++;
     }
