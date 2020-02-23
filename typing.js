89b1bfc4ec3a022fea8c71e2318324ede66fabe9
Mathias007 <mathias.najlepszy@gmail.com>
2020-02-23 19:15:32 +0100

    Optimize the typing function.

--- typing.js
+++ typing.js
@@ -11,15 +11,18 @@ let oldTime = 0;
 let speed = 100; // im większa wartość, tym wolniejsze pisanie
 
 const typing = newTime => {
-    if (wordIndex === text[textIndex].length) {
-        return setTimeout(() => {
-            box.textContent = "";
-            textIndex++;
-            wordIndex = 0;
-            requestAnimationFrame(typing);
-        }, 2000);
-    }
     if (newTime - oldTime > speed) {
+        if (wordIndex === text[textIndex].length) {
+            if (textIndex === text.length - 1) {
+                return;
+            }
+            return setTimeout(() => {
+                box.textContent = "";
+                textIndex++;
+                wordIndex = 0;
+                requestAnimationFrame(typing);
+            }, 2000);
+        }
         oldTime = newTime;
         const letter = text[textIndex].substr(wordIndex, 1);
         box.textContent += letter;
